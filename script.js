cd50bc5db9d19aae0948a8b992400ddd277ec365
Alejandro F <afuste7@gmail.com>
2020-02-08 23:11:33 -0500

    added padding to form and comments to script file

---
+++ script.js
@@ -0,0 +1,28 @@
+// Create event listener for button click
+
+// Get text value from input field and store in global variable
+
+// Store text value in local storage
+
+// Get text value from local storage
+
+// Create click events for past searches
+
+// Prepend text value into div for past searches
+
+// Create query URL with text value from global variable and imperial value
+
+// Create ajax call
+
+// Console log response
+
+// Get temperature, humidity, wind speed, uv index info from respone
+
+// Get city, date, and icon to represent current weather condition
+
+/* Dynamically input city, date, icon, temp, humidity, wind speed, 
+and uv index into city div */
+
+// Color code uv index
+
+// create 5-day forecast card with date, weather condition icon, temp, humidity
