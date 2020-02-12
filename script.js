4e627f88400cac27f24650f3bfcfc1436e84191e
Alejandro F <afuste7@gmail.com>
2020-02-12 16:24:38 -0500

    added values for current weather, ajax call for 5-day forcast, and pulled values from forecast

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 	var apiKey = '476789b4ea7a5f8db7a7b4f6f0c734fb';
 	var searchDiv = $('#searches');
 	var aside = $('.col-sm-4');
-	console.log(aside);
-
+	var currentDiv = $('#weatherDiv');
+	var fiveDayDiv = $('#forecastDiv');
 	// Create event listener for button click
 
 	$('#searchBtn').on('click', function(e) {
@@ -71,7 +71,7 @@ $(document).ready(function() {
 
 		var queryURL = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=' + pcity + '&appid=' + apiKey;
 
-		// Create ajax call
+		// Create ajax call for current weather
 
 		$.ajax({
 			url: queryURL,
@@ -79,17 +79,78 @@ $(document).ready(function() {
 		}).then(function(res) {
 			// Console log response
 
-			console.log(res);
-			console.log(res.weather);
+			// console.log(res);
+
+			// Get city, date, and icon to represent current weather condition
+			let cityV = res.name;
+			let date = res.dt;
+			// let dateString = date.toUTCString();
+			// console.log(dateString);
+
+			// Get temperature, humidity, wind speed, uv index info from response
+			let temp = res.main.temp;
+			let hum = res.main.humidity;
+			let windSp = res.wind.speed;
+			// let uv = res.
+			let weatherIcon = res.weather[0].icon;
+			var icon = '';
+
+			// console.log(cityV, date, temp, hum, windSp, weatherIcon);
+
+			/* Dynamically input city, date, icon, temp, humidity, wind speed, 
+			and uv index into city div */
+
+			var firstDiv = $('<div>').attr('id', 'firstRow');
+			var inputCity = $('<h3>').text(cityV);
+			var dateEl = $('<h3>').text('date');
+			var wIcon = $('<p>').text(weatherIcon);
+			var firstRow = firstDiv.append(inputCity, dateEl, wIcon);
+
+			var secondDiv = $('<div>').attr('id', 'secondRow');
+			var tempDiv = $('<p>').text('Temperature: ' + temp + ' °F');
+
+			var thirdDiv = $('<div>').addClass('rows');
+			var humDiv = $('<p>').text('Humidity: ' + hum + ' %');
+			// var thirdRow = thirdDiv.append(humDiv);
+
+			var fourthDiv = $('<div>').addClass('rows');
+			var windSpeed = $('<p>').text('Wind Speed: ' + windSp + ' MPH');
+			// var fourthRow = fourthDiv.append(windSpeed);
+
+			var fifthDiv = $('<div>').addClass('rows');
+			var uvIndex = $('<span><p></p></span>').text('UV Index: ' + 'object w/ temp');
+			// var fifthRow = fifthDiv.append(uvIndex);
+
+			var secondRow = secondDiv.append(tempDiv, humDiv, windSpeed, uvIndex);
+
+			currentDiv.append(firstRow, secondRow);
+		});
+
+		// Create query URL with text value from global variable and imperial value
+
+		var queryURL =
+			'https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=' + pcity + '&appid=' + apiKey;
+
+		// Create ajax call for 5 day forecast
+
+		$.ajax({
+			url: queryURL,
+			method: 'GET'
+		}).then(function(resp) {
+			// Console log response
+
+			console.log(resp);
+
+			for (i = 0; i < 6; i++) {
+				var dateFor = resp.list[i].dt;
+				var iconFor = resp.list[i].weather.icon;
+				var tempFor = resp.list[i].main.temp;
+				var humFor = resp.list[i].main.humidity;
+
+				console.log(dateFor, iconFor, tempFor, humFor);
+			}
 		});
 	});
-
-	// Get temperature, humidity, wind speed, uv index info from response
-
-	// Get city, date, and icon to represent current weather condition
-
-	/* Dynamically input city, date, icon, temp, humidity, wind speed, 
-and uv index into city div */
 
 	// Color code uv index
 
